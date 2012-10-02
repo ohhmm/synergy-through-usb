e967487f829a747236ab5e9c2d15ff92adfd94fe
Sergei Krivonos <sergeikrivonos@gmail.com>
2012-10-02 13:03:27 +0300

    Added CUSBDataLink stub

---
+++ src/lib/net/CUSBDataLinkListener.h
@@ -0,0 +1,42 @@
+/*
+ * synergy -- mouse and keyboard sharing utility
+ * Copyright (C) 2012 Bolton Software Ltd.
+ * Copyright (C) 2002 Chris Schoeneman
+ * 
+ * This package is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * found in the file COPYING that should have accompanied this file.
+ * 
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CUSBDataLinkListener_H
+#define CUSBDataLinkListener_H
+
+#include "IListenSocket.h"
+
+//! USB data link cable listen
+/*!
+ Data link connection listener class.
+*/
+class CUSBDataLinkListener : public IListenSocket {
+public:
+	CUSBDataLinkListener();
+	~CUSBDataLinkListener();
+
+	// ISocket overrides
+	virtual void		bind(const CNetworkAddress&);
+	virtual void		close();
+	virtual void*		getEventTarget() const;
+
+	// IListenSocket overrides
+	virtual IDataTransfer*	accept();
+};
+
+#endif
