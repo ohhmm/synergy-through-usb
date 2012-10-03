b7d42fd37750fd12637d7186abd77084e312486d
JOPA <JOPA@amd.com>
2012-10-03 16:21:03 +0300

    CUSBDataLink partially impl

--- src/lib/arch/CArchUsbDataLink.cpp
+++ src/lib/arch/CArchUsbDataLink.cpp
@@ -1,2 +1,93 @@
+#include "XArch.h"
 #include "CArchUsbDataLink.h"
 #include <libusb.h>
+
+CArchUsbDataLink::CArchUsbDataLink() :
+	m_usbContext(NULL)
+{
+}
+
+CArchUsbDataLink::~CArchUsbDataLink()
+{
+	usbShut();
+}
+
+//
+// CONTEXT
+//
+
+void CArchUsbDataLink::init()
+{
+	usbInit();
+}
+
+void CArchUsbDataLink::usbInit()
+{
+	usbShut();
+
+	int rc = libusb_init(&m_usbContext);
+	if (rc != 0)
+	{
+		m_usbContext = NULL;
+		throw XArchNetwork("libusb_init failed");
+	}
+}
+
+void CArchUsbDataLink::usbShut()
+{
+	if (m_usbContext)
+	{
+		libusb_exit(m_usbContext);
+		m_usbContext = NULL;
+	}
+}
+
+USBContextHandle CArchUsbDataLink::usbGetContext()
+{
+	return m_usbContext;
+}
+
+//
+// DEVICE
+//
+
+USBDeviceHandle CArchUsbDataLink::usbOpenDevice(int vid, int pid, int ifid)
+{
+	USBDeviceHandle dev = libusb_open_device_with_vid_pid(m_usbContext, vid, pid);
+	if (!dev)
+	{
+		throw XArchNetwork("libusb_open_device_with_vid_pid failed");
+	}
+
+	int rc = libusb_claim_interface(dev, ifid);
+	if (rc != 0)
+	{
+		libusb_close(dev);
+		throw XArchNetwork("libusb_claim_interface failed");
+	}
+
+	return dev;
+}
+
+void CArchUsbDataLink::usbCloseDevice(USBDeviceHandle dev, int ifid)
+{
+	if (dev)
+	{
+		libusb_release_interface(dev, ifid);
+		libusb_close(dev);
+	}
+}
+
+int CArchUsbDataLink::usbBulkTransfer(USBDeviceHandle dev, bool write, unsigned char port, unsigned char* buf, unsigned int len, unsigned int timeout)
+{
+	unsigned char iomode = write ? LIBUSB_ENDPOINT_OUT : LIBUSB_ENDPOINT_IN;
+
+	int transferred = 0;
+	int rc = libusb_bulk_transfer(dev, port | iomode, buf, len, &transferred, timeout);
+	if (rc != 0)
+	{
+		throw XArchNetwork("libusb_bulk_transfer failed");
+	}
+
+	return transferred;
+}
--- src/lib/arch/IArchUsbDataLink.h
+++ src/lib/arch/IArchUsbDataLink.h
@@ -21,13 +21,34 @@
 
 #include "common/IInterface.h"
 
+typedef struct libusb_context* USBContextHandle;
+typedef struct libusb_device_handle* USBDeviceHandle;
+
+struct USBDataLinkConfig
+{
+	int vid;
+	int pid;
+	int ifid;
+	int bulkin;
+	int bulkout;
+};
+
 //! Interface for architecture dependent USB
 /*!
 This interface defines the USB transport operations required by
 synergy.  Each architecture must implement this interface.
 */
 class IArchUsbDataLink : public IInterface {
+public:
+	virtual void init() = 0;
 
+	virtual void usbInit() = 0;
+	virtual void usbShut() = 0;
+	virtual USBContextHandle usbGetContext() = 0;
+
+	virtual USBDeviceHandle usbOpenDevice(int vid, int pid, int ifid) = 0;
+	virtual void usbCloseDevice(USBDeviceHandle dev, int ifid) = 0;
+	virtual int usbBulkTransfer(USBDeviceHandle dev, bool write, unsigned char port, unsigned char* buf, unsigned int len, unsigned int timeout) = 0;
 };
 
 #endif
