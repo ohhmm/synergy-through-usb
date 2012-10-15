55740413ed89bfac7580fdd6f7fbb26f76f4455e
SHLAK <shlak.com>
2012-10-15 11:20:57 +0300

    teper' vse zaibis'

--- src/lib/net/CUSBDataLink.h
+++ src/lib/net/CUSBDataLink.h
@@ -80,6 +80,7 @@ private:
 	libusb_transfer*	m_transferWrite;
 
 	Mutex				m_mutex;
+	char				m_writeBuffer[1024*1024];
 	char				m_readBuffer[1024*1024];
 	StreamBuffer		m_inputBuffer;
 	StreamBuffer		m_outputBuffer;
